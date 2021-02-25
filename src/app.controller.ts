37d8b74da73bc94f28b3eb2d00a8e0e125af52c1
Dup4 <lyuzhi.pan@gmail.com>
2021-02-25 13:30:28 +0800

    feat: version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
@@ -8,5 +8,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("h")
+  getH(): string {
+    return "h";
+  }
+
+  @Get("hh")
+  getHH(): string {
+    return "hh";
+  }
+
+  @Get("version")
+  getVersion(): string {
+    return this.appService.getVersion();
   }
 }
