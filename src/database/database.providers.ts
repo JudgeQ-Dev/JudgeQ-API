3e93682d849d3e0d4fd6a3bfa457487ab4b54df2
Dup4 <lyuzhi.pan@gmail.com>
2021-03-01 13:35:15 +0800

    feat: config

---
+++ src/database/database.providers.ts
@@ -0,0 +1,20 @@
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { ConfigService } from "@/config/config.service";
+
+export const databaseProviders = [
+  TypeOrmModule.forRootAsync({
+    useFactory: (configService: ConfigService) => ({
+      type: configService.config.services.database.type,
+      host: configService.config.services.database.host,
+      port: configService.config.services.database.port,
+      username: configService.config.services.database.username,
+      password: configService.config.services.database.password,
+      database: configService.config.services.database.database,
+      entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+      logging: !!process.env.SYZOJ_NG_LOG_SQL,
+      synchronize: true
+    }),
+    inject: [ConfigService]
+  })
+];
