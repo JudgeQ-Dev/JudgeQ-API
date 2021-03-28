8d4500df905373170ee06bd0b7539a35d7b41eee
Dup4 <lyuzhi.pan@gmail.com>
2021-03-28 15:39:27 +0800

    fix: submit await

--- src/submission/submission.service.ts
+++ src/submission/submission.service.ts
@@ -325,7 +325,8 @@ export class SubmissionService implements JudgeTaskService<SubmissionProgress, S
   > {
     const problemTypeService = this.problemTypeFactoryService.type(problem.type);
 
-    await new Promise(r => setTimeout(r, 10000));
+    // TODO: Don't know why to do this.
+    // await new Promise(r => setTimeout(r, 10000));
 
     const validationError = await problemTypeService.validateSubmissionContent(content);
     if (validationError && validationError.length > 0) return [validationError, null, null];
