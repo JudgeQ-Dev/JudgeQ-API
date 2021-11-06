3449c39a750ba7bf46cf1f4d0b7c07db3850a4de
Dup4 <lyuzhi.pan@gmail.com>
2021-11-06 17:23:11 +0800

    feat: add post_build script

---
+++ post_build.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+SRC_DIR="${TOP_DIR}"/src
+DIST_DIR="${TOP_DIR}"/dist
+
+DIR_LIST=auth/scripts mail/templates redis/scripts
+
+# ensure that the required resource files are copied
+for dir in ${DIR_LIST}; do
+  cp -a "${SRC_DIR}/${dir}" "${DIST_DIR}/${dir}"
+done
