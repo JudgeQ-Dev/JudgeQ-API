35bce1b88f0c9b294988600e1e9da33e99e1dcca
Dup4 <lyuzhi.pan@gmail.com>
2022-05-28 21:28:06 +0800

    fix: config path

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 export NODE_ENV=production
-export JUDGEQ_CONFIG_FILE=/app/config.yaml
+export JUDGEQ_CONFIG_FILE=/app/config/config.yaml
 
 if [[ X"${1}" = X"primary" ]]; then
   exec node /app/dist/main.js
