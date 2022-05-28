190d13ce2455515c75b604bb839e1f26cae89abe
Dup4 <lyuzhi.pan@gmail.com>
2022-05-28 09:05:30 +0800

    chore: use pnpm in docker build

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -5,7 +5,7 @@ set -e -x
 export NODE_ENV=production
 export JUDGEQ_CONFIG_FILE=/root/config.yaml
 
-if [[ X"${1}" = X"judgeq-api" ]]; then
+if [[ X"${1}" = X"judgeq-server" ]]; then
   exec node /root/dist/main.js
 else
   exec "$@"
