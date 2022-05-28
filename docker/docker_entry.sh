8686f85146143bc00ac2c0daefa4bc68cfa3397b
Dup4 <lyuzhi.pan@gmail.com>
2022-05-28 23:57:56 +0800

    chore: use sh in docker runtime

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#! /bin/sh
 
 set -e -x
 
 export NODE_ENV=production
 export JUDGEQ_CONFIG_FILE=/app/config/config.yaml
 
-if [[ X"${1}" = X"primary" ]]; then
+if [ X"${1}" = X"primary" ]; then
   exec node /app/dist/main.js
 else
   exec "$@"
